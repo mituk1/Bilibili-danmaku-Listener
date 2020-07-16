4b7659ba4eade54048d3caebca6f98c47a029fa6
Saki9 <765246354@qq.com>
2020-07-16 16:53:57 +0800

    将包含多条 message 的字符串拆分展示

--- src/main/java/com/saki9/danmaku/service/MessageHandleService.java
+++ src/main/java/com/saki9/danmaku/service/MessageHandleService.java
@@ -2,7 +2,9 @@ package com.saki9.danmaku.service;
 
 import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.zip.DataFormatException;
 import java.util.zip.Inflater;
 
@@ -12,26 +14,49 @@ import java.util.zip.Inflater;
 public class MessageHandleService {
 
     public void messageHandle(ByteBuffer message) throws DataFormatException {
-        String s = messageToJson(message);
-        System.out.println(s);
+        List<String> s = messageToJson(message);
+        for (String s1 : s) {
+            System.out.println(s1);
+        }
     }
 
-    public String messageToJson(ByteBuffer message) throws DataFormatException {
+    private List<String> messageToJson(ByteBuffer message) throws DataFormatException {
         byte[] messageBytes = message.array();
         byte[] mainMessageBytes = Arrays
             .copyOfRange(messageBytes, 16, messageBytes.length);
 
         if (messageBytes[16] != 120) {
-            return new String(mainMessageBytes, StandardCharsets.UTF_8);
+            return Arrays.asList(new String(mainMessageBytes, StandardCharsets.UTF_8));
         }
 
         // 解压缩弹幕信息
         byte[] newByte = new byte[1024 * 5];
         Inflater inflater = new Inflater();
         inflater.setInput(mainMessageBytes);
-        int inflate = inflater.inflate(newByte);
-        newByte = Arrays.copyOfRange(newByte, 16, inflate);
-        return new String(newByte, StandardCharsets.UTF_8);
+        newByte = Arrays.copyOfRange(newByte, 16, inflater.inflate(newByte));
+        return splitStringToJson(new String(newByte, StandardCharsets.UTF_8));
+    }
+
+    private static List<String> splitStringToJson(String str) {
+        List<String> result = new ArrayList<>();
+        for (int i = 1, count = 1; i < str.length(); i++) {
+
+            if (str.charAt(i) == '{') {
+                count++;
+            } else if (str.charAt(i) == '}') {
+                count--;
+            }
+
+            if (count == 0) {
+                result.add(str.substring(0, i + 1));
+                int nextIndex = str.indexOf("{", i + 1);
+                if (nextIndex != -1) {
+                    result.addAll(splitStringToJson(str.substring(nextIndex)));
+                }
+                return result;
+            }
+        }
+        return result;
     }
 
 }
